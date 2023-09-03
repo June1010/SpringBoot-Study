ad655b05659c6f251769860ea7e5f967d11a13bc
June1010 <tjrwns1010@naver.com>
2023-09-03 15:09:50 +0900

    ~ Section 5. API 개발 고급(완)

--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -1,23 +1,11 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.util.StringUtils;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter @Setter
-@RequiredArgsConstructor
 public class OrderSearch {
-    private final EntityManager em;
 
     private String memberName; // 회원 이름
     private OrderStatus orderStatus; // 주문 상태[ORDER, CANCEL]
